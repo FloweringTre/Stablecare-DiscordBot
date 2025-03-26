ab3a8131136d585434a2fbc97be1808a9aaac8bc
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-03-26 16:54:34 -0700

    Added Stablecare commands
    added NumPy to handle arrays
    added variables for handling pony variables
    added commands to handle creating the pony, feeding, giving treats, watering, and vet care for the pony

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -44,6 +45,49 @@ async def printer(interaction: discord.interaction, printer: str):
     #now printer is a variable that is accepted by the slash command with the accepted type of a string
     #the variable name is the value of the little black box in discord
     await interaction.response.send_message(printer)
+
+
+#Stablecare bot specific commands
+PONY_NAME : str = "Pony"
+PONY_PRONOUN : int = 2
+PRONOUNS = np.array([[she, her, hers, hers], [he, him, his, his], [they, them, their, theirs]])
+
+@client.tree.command(name-"create_pony", description="Set up your pony! Gender: 0-Mare 1-Gelding/Stallion 2-Use 'They' pronouns", guild=GUILD_ID)
+async def createAPony(interaction: discord.interaction, pony_name: str, pony_gender : int):
+    if pony_gender > 2 or pony_gender < 0:
+        await interaction.response.send_message(f'Please try again and properly select a gender for your pony (0-2)')
+    else:
+        PONY_NAME = pony_name
+        PONY_PRONOUN = pony_gender
+        await interaction.response.send_message(f'Congrats! {PONY_NAME} has come home to you! Please take good care of {PRONOUNS[PONY_PRONOUN,1]}. :horse:')
+
+
+@client.tree.command(name-"treats", description="Give your pony a treat. Type in whatever treat you want to feed your pony!", guild=GUILD_ID)
+async def treatSnacking(interaction: discord.interaction, treat_type: str):
+    await interaction.response.send_message(f'{PONY_NAME} snacks on {treat_type}. {PRONOUNS[PONY_PRONOUN,0]} loved it!')
+
+@client.tree.command(name-"feed", description="Feed your pony. Type in whatever hay type and pounds you want to feed your pony", guild=GUILD_ID)
+async def foodTime(interaction: discord.interaction, feed_type: str, feed_amount: int):
+    await interaction.response.send_message(f'You feed {PONY_NAME} {feed_amount}lbs of {feed_type}. {PRONOUNS[PONY_PRONOUN,0]} are full and satisfied.')
+
+@client.tree.command(name-"water", description="Fills your pony's water bucket", guild=GUILD_ID)
+async def waterTime(interaction: discord.interaction):
+    await interaction.response.send_message(f'{PONY_NAME}\'s water bucket is now full. {PRONOUNS[PONY_PRONOUN,0]} takes a nice long sip.')
+
+@client.tree.command(name-"vet_care", description="Heal your pony. Services Menu: 1-Vaccines 2-Dental 3-Check up", guild=GUILD_ID)
+async def vetServices(interaction: discord.interaction, vet_services: int):
+    if vet_services = 1:
+        await interaction.response.send_message(f'{PONY_NAME} stood for {PRONOUNS[PONY_PRONOUN,2]} shots. {PRONOUNS[PONY_PRONOUN,0]} were a very brave pony!')
+    if vet_services = 2:
+        await interaction.response.send_message(f'The dental floating went well. {PRONOUNS[PONY_PRONOUN,2]} teeth are nice and flat now, no more mouth issues for {PONY_NAME}!')
+    if vet_services = 3:
+        await interaction.response.send_message(f'The vet did a once over on {PONY_NAME}. They said {PONY_NAME} is the picture of health!')
+    else:
+        await interaction.response.send_message("The vet can't provide services if you don't select any. Please select a service from the list (1-3)")
+
+
+
+
 
 client = Client(intents=intents)
 client.run('')
