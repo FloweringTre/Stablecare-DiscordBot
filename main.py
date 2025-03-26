df730a688a6104be9a4bfbae0208bf41f2365aa6
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-03-25 20:54:22 -0700

    updated with reaction and message monitoring

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ class Client(discord.Client):
         print(f'Successfully connected as {self.user}.')
     
     async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-    
-    
+        if message.author == self.user:
+            return
+        if message.content.startswith('hello'):
+            await message.channel.send(f'Hello there {message.author} :horse: :heart:')
+
+    async def on_reaction_add(self, reaction, user):
+        await reaction.message.channel.send('You reacted')
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run('MTM1NDI4OTY1OTUxNzA3NTUzOA.GHXmTU.aqvjws_v3TBG_R9GZ-KupzMEW7MLFERIvXSt6c')
+client.run('')
