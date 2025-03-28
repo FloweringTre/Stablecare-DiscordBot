02462e47a88e6f14f40cfe8afbce12530c5a2e2c
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-03-27 21:55:34 -0700

    Fully sync with the SQL server
    - patched code to function with the sql server for data storing
    - added brushing command for increasing cleanliness stat
    - lots of little bug fixes with spelling and punctuation
    - patched code so functions will run correctly in the slash commands
    - added the dental floating text back in to the vet services slash command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ SERVER = 0 #add testing discord server id here
 def connect_db():
     return mysql.connector.connect(
         #Database Values
+
     )
 
 #############################################################################
@@ -17,6 +18,8 @@ def connect_db():
 class Client(commands.Bot):
     async def on_ready(self): #on launch
         print(f'Successfully connected as {self.user}.')
+        print("successfully finished startup")
+        
 
         try: #force syncing with the dev server to test commands
             guild=discord.Object(id=SERVER)
@@ -35,7 +38,7 @@ class Client(commands.Bot):
 
     async def on_reaction_add(self, reaction): #reading and responding to reactions
         await reaction.message.channel.send('You reacted... terminating bot')
-        await close()
+        quit()
 
     ########################################################################################
     ################################# STABLECARE FUNCTIONS #################################
@@ -44,7 +47,7 @@ class Client(commands.Bot):
         conn = connect_db()
         cursor = conn.cursor()
 
-        SELECTION_STR = (f-'SELECT * from horse_information WHERE user_id = {user_id}')
+        SELECTION_STR = (f'SELECT * from horse_information WHERE user_id = {user_id}')
         
         cursor.execute(SELECTION_STR)
         horse_data = cursor.fetchone()
@@ -54,57 +57,55 @@ class Client(commands.Bot):
         return horse_data
     
     ### update table information for a horse
-    async def update_horse_data(user_id, data_column, updated_value)
+    async def update_horse_data(user_id, data_column, updated_value):
         try:
             conn = connect_db()
             cursor = conn.cursor()
 
-            QUERY_STR = (f-'UPDATE horse_information SET {data_column}  = ? WHERE user_id = ?')
+            QUERY_STR = (f"UPDATE horse_information SET {data_column} = %s WHERE user_id = %s")
             
             cursor.execute(QUERY_STR, (updated_value, user_id))
-            conn.commit
-            
+            conn.commit()
+            print(f"The good query: {QUERY_STR}")
+
             conn.close()
             return True
 
-        except mysql.Error as e:
+        except mysql.connector.Error as e:
             print(f"An error has happened while attempting to update data: {e}")
+            print(f"The bad query: {QUERY_STR}")
             return False
 
     ### update all table values to reduce horse feed and water stats
-    async def daily_horse_update(self)
+    async def daily_horse_update():
         try:
             conn = connect_db()
             cursor = conn.cursor()
             
-            #update hunger
+            #reduce stat values
             cursor.execute("UPDATE horse_information SET hunger = hunger - 3")
-            conn.commit
-            cursor.execute("UPDATE horse_information SET hunger = 0 WHERE hunger < 0")
-            conn.commit
-
-            #update thirst
             cursor.execute("UPDATE horse_information SET thirst = thirst - 5")
-            conn.commit
-            cursor.execute("UPDATE horse_information SET thirst = 0 WHERE thirst < 0")
-            conn.commit
-
-            #update cleanilness
             cursor.execute("UPDATE horse_information SET clean = clean - 3")
-            conn.commit
-            cursor.execute("UPDATE horse_information SET clean = 0 WHERE clean < 0")
-            conn.commit
+            conn.commit()
 
-            #update health
+            #ensure no stat is below zero
+            cursor.execute("UPDATE horse_information SET hunger = 0 WHERE hunger < 0")
+            cursor.execute("UPDATE horse_information SET thirst = 0 WHERE thirst < 0")
+            cursor.execute("UPDATE horse_information SET clean = 0 WHERE clean < 0")
+            conn.commit()
+
+            #update health based on other values
             cursor.execute("UPDATE horse_information SET health = health - 2 WHERE hunger <= 5 AND thirst <= 5")
-            conn.commit
+            conn.commit()
+
+            #ensure health is not below zero
             cursor.execute("UPDATE horse_information SET health = 0 WHERE health < 0")
-            conn.commit
+            conn.commit()
 
             conn.close()
             return True
             
-        except mysql.Error as e:
+        except mysql.connector.Error as e:
             print(f'An error has happened while attempting to update all horses values: {e}')
             return False
 
@@ -138,7 +139,7 @@ async def printer(interaction: discord.Interaction, printer: str):
 #demo for how to build an embed
 @client.tree.command(name="embed", description="Demo for embed messages", guild=GUILD_ID)
 async def embedDemo(interaction: discord.Interaction): 
-    embed = discord.Embed(title="I am a title", url="https://www.google.com", discripton="I am a description", color=discord.Color.teal)
+    embed = discord.Embed(title="I am a title", url="https://www.google.com", description="I am a description")
     #url will turn the title to a link
     #color can be any value so long as you specify it (RGB/HEX/etc) - currently using the preset colors from discord.py
 
@@ -161,19 +162,23 @@ async def embedDemo(interaction: discord.Interaction):
 ################################# MANUALLY PUSH THE UPDATE TO ALL HORSE STATS AND RETURN YOUR HORSE'S STATS #################################
 @client.tree.command(name="dailyupdate", description="Updates all horse values", guild=GUILD_ID)
 async def runDailyUpdate(interaction: discord.Interaction):
-    result = self.daily_horse_update()
+    result = await Client.daily_horse_update()
     if result:
-        user_id = interaction.user_id
-        horse_data = self.gather_all_horse_data(user_id)
+        user_id = interaction.user.id
+        horse_data = await Client.gather_all_horse_data(user_id)
 
-        if !horse_data:
-            await interaction.response.send_message(f'The update has been run... but you don\'t have a horse to see their updated stats on.')
+        if horse_data:
+            print(horse_data)
+            await interaction.response.send_message("Update complete")
+            #await interaction.response.send_message(f'The update has been run. Your horse {horse_data[2]} has the following stats.')
+            #await interaction.response.send_message(f'Hunger: {horse_data[5]}')
+            #await interaction.response.send_message(f'Thirst: {horse_data[6]}')
+            #await interaction.response.send_message(f'Cleanliness: {horse_data[7]}')
+            #await interaction.response.send_message(f'Health: {horse_data[4]}')
+
         else:
-            await interaction.response.send_message(f'The update has been run. Your horse {horse_data[2]} has the following stats.')
-            await interaction.response.send_message(f'Hunger: {horse_data[5]}')
-            await interaction.response.send_message(f'Thirst: {horse_data[6]}')
-            await interaction.response.send_message(f'Cleanliness: {horse_data[7]}')
-            await interaction.response.send_message(f'Health: {horse_data[4]}')
+            await interaction.response.send_message(f'The update has been run... but you don\'t have a horse to see their updated stats on.')
+
     else:
         await interaction.response.send_message(f'An error occurred while trying to run the update.')
 
@@ -189,8 +194,9 @@ async def runDailyUpdate(interaction: discord.Interaction, confirmation_to_remov
             conn = connect_db()
             cursor = conn.cursor()
             
-            cursor.execute(f'DELETE FROM horse_information WHERE user_id = {user_id}')
-            conn.commit
+            QUERY = (f'DELETE FROM horse_information WHERE user_id = {user_id}')
+            cursor.execute(QUERY)
+            conn.commit()
 
             conn.close()
 
@@ -219,22 +225,27 @@ async def createAPony(interaction: discord.Interaction, pony_name: str, pony_gen
         await interaction.response.send_message(f'Please try again and properly select a gender for your pony (0-2)')
     else:
         user_id = interaction.user.id
-        user_name = interaaction.user.display_name
+        user_name = interaction.user.display_name
         conn = connect_db() #connect to the database
         cursor = conn.cursor() #start work on the database. cursor will be the action to move through the database
         
         # Check if the user already exists in the database - cursor.execute will run a sql fn in the database
-        cursor.execute("SELECT * FROM horse_information WHERE user_id = %s", (user_id,))
+        cursor.execute(f"SELECT * FROM horse_information WHERE user_id = {user_id}")
         user = cursor.fetchone() #fetchone() gets the first row of the results
 
         if user: #if something was pulled above...
-            await interaction.response.send_message(f'{interaction.user.id}, you already have a horse!')
+            await interaction.response.send_message(f'Sorry {interaction.user.display_name}, you already have a horse!')
         else:
             # Insert user data into the database
-            cursor.execute("INSERT INTO horse_information (user_id, user_name, horse_name, gender, health, hunger, thirst, clean, money) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
-                        (user_id, user_name, pony_name, pony_gender, 10, 10, 10, 10, 30))
-            conn.commit() #commits the information above to the database to save the addition of information
-            await interaction.response.send_message(f'Congrats! {pony_name} has come home to you! Please take good care of {PRONOUNS[pony_gender,1]}. :horse:')
+            #await interaction.response.send_message("The SQL server does not have a horse registered to you.")
+            try:
+                QUERY = (f"INSERT INTO horse_information VALUES ({user_id}, \"{user_name}\", \"{pony_name}\", {pony_gender}, 10, 10, 10, 10, 30)")
+                cursor.execute(QUERY)
+                conn.commit() #commits the information above to the database to save the addition of information
+                await interaction.response.send_message(f'Congrats! {pony_name} has come home to you! Please take good care of {PRONOUNS[pony_gender,1]}. :horse:')
+            except mysql.connector.Error as e:
+                print(f'Error occurred while attempting to add a horse for {user_name}: {e}')
+                await interaction.response.send_message(f'An error has occured while attempting to add your horse to our stable. Please contact an adminstrator for assistance.')
 
         conn.close() #safely exit the database connection
 
@@ -242,46 +253,62 @@ async def createAPony(interaction: discord.Interaction, pony_name: str, pony_gen
 ################################# HORSE CARE COMMANDS #################################
 @client.tree.command(name="treats", description="Give your pony a treat. Type in whatever treat you want to feed your pony!", guild=GUILD_ID)
 async def treatSnacking(interaction: discord.Interaction, treat_type: str):
-    user_id = interaction.user_id
-    horse_data = self.gather_all_horse_data(user_id)
+    user_id = interaction.user.id
+    horse_data = await Client.gather_all_horse_data(user_id)
 
     await interaction.response.send_message(f'{horse_data[2]} snacks on {treat_type}. {PRONOUNS[horse_data[3],0]} loved it!')
 
 @client.tree.command(name="feed", description="Feed your pony. Type in whatever hay type and pounds you want to feed your pony", guild=GUILD_ID)
 async def foodTime(interaction: discord.Interaction, feed_type: str, feed_amount: int):
-    user_id = interaction.user_id
-    horse_data = self.gather_all_horse_data(user_id)
-    update = update_horse_data(user_id, hunger, 10)
-    if update == false:
+    user_id = interaction.user.id
+    horse_data = await Client.gather_all_horse_data(user_id)
+    update = await Client.update_horse_data(user_id, "hunger", 10)
+    if update == False:
         await interaction.response.send_message(f'Sorry, {interaction.user.display_name}, we don\'t have a horse registered to you')
-    else
-    await interaction.response.send_message(f'You feed {horse_data[2]} {feed_amount}lbs of {feed_type}. {PRONOUNS[horse_data[3],0]} are full and satisfied.')
+    else:
+        await interaction.response.send_message(f'You feed {horse_data[2]} {feed_amount}lbs of {feed_type}. {PRONOUNS[horse_data[3],0]} are full and satisfied.')
 
 @client.tree.command(name="water", description="Fills your pony's water bucket", guild=GUILD_ID)
 async def waterTime(interaction: discord.Interaction):
-    user_id = interaction.user_id
-    horse_data = self.gather_all_horse_data(user_id)
-    update = update_horse_data(user_id, thirst, 10)
-    if update == false:
+    user_id = interaction.user.id
+    horse_data = await Client.gather_all_horse_data(user_id)
+    update = await Client.update_horse_data(user_id, "thirst", 10)
+    print(f'{horse_data}')
+    if update == False:
         await interaction.response.send_message(f'Sorry, {interaction.user.display_name}, we don\'t have a horse registered to you')
     else:
         await interaction.response.send_message(f'{horse_data[2]}\'s water bucket is now full. {PRONOUNS[horse_data[3],0]} takes a nice long sip.')
 
+@client.tree.command(name="brush", description="Brush your pony's coat", guild=GUILD_ID)
+async def brushTime(interaction: discord.Interaction):
+    user_id = interaction.user.id
+    horse_data = await Client.gather_all_horse_data(user_id)
+    update = await Client.update_horse_data(user_id, "clean", 10)
+    print(f'{horse_data}')
+    if update == False:
+        await interaction.response.send_message(f'Sorry, {interaction.user.display_name}, we don\'t have a horse registered to you')
+    else:
+        await interaction.response.send_message(f'{horse_data[2]} enjoys being brushed. {PRONOUNS[horse_data[3],2]} coat is now spotless!')
+
+
 @client.tree.command(name="vetcare", description="Heal your pony. Services Menu: 1-Vaccines 2-Dental 3-Check up", guild=GUILD_ID)
 async def vetServices(interaction: discord.Interaction, vet_services: int):
-    user_id = interaction.user_id
-    horse_data = self.gather_all_horse_data(user_id)
-    update = update_horse_data(user_id, health, 10)
-    if update == false:
+    user_id = interaction.user.id
+    horse_data = await Client.gather_all_horse_data(user_id)
+    update = await Client.update_horse_data(user_id, "health", 10)
+    if update == False:
         await interaction.response.send_message(f'Sorry, {interaction.user.display_name}, we don\'t have a horse registered to you')
-    else
-        if vet_services == 1:
-            await interaction.response.send_message(f'{horse_data[2]} stood for {PRONOUNS[horse_data[3],2]} shots. {PRONOUNS[horse_data[3],0]} were a very brave pony!')
-        if vet_services == 2:
-        if vet_services == 3:
-            await interaction.response.send_message(f'The vet did a once over on {horse_data[2]}. They said {horse_data[2]} is the picture of health!')
-        else:
-            await interaction.response.send_message("The vet can't provide services if you don't select any. Please select a service from the list (1-3)")
+    else:
+        print(f'{horse_data[2]}: {PRONOUNS[horse_data[3],2]} health is {horse_data[4]}')
+        match vet_services:
+            case 1:
+                await interaction.response.send_message(f'{horse_data[2]} stood for {PRONOUNS[horse_data[3],2]} shots. {PRONOUNS[horse_data[3],0]} were a very brave pony!')
+            case 2:
+                await interaction.response.send_message(f'The dental floating went well. {PRONOUNS[horse_data[3],2]} teeth are nice and flat now, no more mouth issues for {horse_data[2]}!')
+            case 3:
+                await interaction.response.send_message(f'The vet did a once over on {horse_data[2]}. They said {horse_data[2]} is the picture of health!')
+            case _:
+                await interaction.response.send_message("The vet can't provide services if you don't select any. Please select a service from the list (1-3)")
 
 
 
