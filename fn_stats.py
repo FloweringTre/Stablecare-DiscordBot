04cb5cfa866a83f07ae8b52e000f2a9eeed06365
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2025-04-11 10:33:55 -0700

    create a fetch image function

--- fn_stats.py
+++ fn_stats.py
@@ -4,7 +4,7 @@ import random
 from typing import Any
 import discord
 
-from data_pull import *
+from fn_data_pull import *
 
 ########################################################################################
 ################################# STABLECARE FUNCTIONS #################################
@@ -56,4 +56,66 @@ async def stat_string(horse_data):
     STAT_STRING = health + f'\n' + hunger + f'\n' + thirst + f'\n' + clean
     return STAT_STRING
 
+### Fetch the URL for the image needed by the activity
+async def fetch_image(horse_data, image_type):
+    #preset the images to be the standard ref incase something goes wrong
+    cust_array_value = 15
+    pre_array_value = 2
+    
+    #find the array location for the image type
+    match image_type:
+        case 0:
+            ## "stand_ref_image"
+            cust_array_value = 15
+            pre_array_value =  2
+        case 1:
+            ## "happy_ref_img"
+            cust_array_value = 16
+            pre_array_value = 3
+        case 2:
+            ## "sad_ref_img"
+            cust_array_value = 17
+            pre_array_value = 4
+        case 3:
+            ## "feed_img"
+            cust_array_value = 18
+            pre_array_value = 5
+        case 4:
+            ## "water_img"
+            cust_array_value = 19
+            pre_array_value = 6
+        case 5:
+            ## "brush_img"
+            cust_array_value = 20
+            pre_array_value = 7
+        case 6:
+            ## "treat_img"
+            cust_array_value = 21
+            pre_array_value = 8
+        case 7:
+            ## "pet_img"
+            cust_array_value = 22
+            pre_array_value = 9
+        case 8:
+            ## "train_img"
+            cust_array_value = 23
+            pre_array_value = 10
+        case 9:
+            ## "show_img"
+            cust_array_value = 24
+            pre_array_value = 11
+
+    image_url = "" #set up variable
+
+    if horse_data[14] == 1: #custom images are active
+        image_url = horse_data[cust_array_value]
+
+        if image_url == "": #if the custom image for that specific value is blank.... 
+            image_url = horse_data[15]  #set to the custom standard ref
+
+    else: #preset images are active
+        coat_values = await gather_coat_values(horse_data[13])
+        image_url = coat_values[pre_array_value]
+    
+    return image_url
 
